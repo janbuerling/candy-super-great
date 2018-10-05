8c5898bdb0e81e5e9d28e028830b2928693135b1
Jan Bürling <jan.buerling@ambient-innovation.com>
2018-10-05 16:30:45 +0200

    try to fix problem with codesandbox.io and fontello.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import  React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/index';
 import './assets/styles/index.scss';
-import './assets/styles/fontello.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
